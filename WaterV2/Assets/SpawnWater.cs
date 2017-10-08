d339104473a767c79cee639309ee46daa51058e3
Alexander Pagliarini <Alexander2.Pagliarini@live.uwe.ac.uk>
2017-10-08 19:38:19 +0100

    aaaa

---
+++ WaterV2/Assets/SpawnWater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWater : MonoBehaviour {
+
+	public GameObject waterParticle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			Instantiate(waterParticle, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), this.transform.localRotation);
+		}
+		if (Input.GetKey (KeyCode.F)) 
+		{
+			
+		}
+	}
+}
